f66b1b3544203d4467b982b0c51cdcd9a8835eba
Andrew <andrewf1993@gmail.com>
2015-10-12 21:08:30 +1300

    Added Geolocation
    Added Geolocation support, including error handling for older browsers.
    Also tidied up the code a little, added some comments.

--- Test/GoogleMaps/mapsScript.js
+++ Test/GoogleMaps/mapsScript.js
@@ -1,19 +1,37 @@
-alert(js_lat[0] + "Maps Script Test");
-alert(js_lat.length + "js_lat.length");
-//TODO: Define myCenter as currentlocation
-var myCenter=new google.maps.LatLng(-45.8728639,170.5094754);
-//center of map. Implement https://developers.google.com/maps/articles/geolocation to get current location;
-var contentString = '<div id="content">'+ '<div id="siteNotice">'+'</div>'+'<h1 id="firstHeading" class="firstHeading">First Heading</h1>'+'<div id="bodyContent">'+'<p><b>Title</b>, </p>'+'Content Stuff' + '</div>'+'</div>';
-function initialize() //intialize the actual map
+//Global Variables
+var defaultCenter = new google.maps.LatLng(-45.8728639,170.5094754); //center of map. Implement https://developers.google.com/maps/articles/geolocation to get current location;
+var map;
+
+//Initialize the actual map
+function initialize()
 {
   var mapProp = {
-    center:myCenter, 
+    center:defaultCenter, 
     zoom:14,
     mapTypeId:google.maps.MapTypeId.ROADMAP,
-	scaleControl: true
+  scaleControl: true
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  
+
+  //Try to detect user's current location
+  var browserSupportFlag = new Boolean(); //Flag used to identify whether user's browser supports geolocation
+  var initialLocation;
+  if (navigator.geolocation)
+  {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+        map.setCenter(initialLocation);
+      }, function() {
+        handleNoGeolocation(browserSupportFlag, map);
+      });
+  }
+  //Else the browser does not support geolocation
+  else
+  {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
   
   var markers = new Array(js_lat.length);
   var infowindows = new Array(js_lat.length);
@@ -23,32 +41,34 @@ function initialize() //intialize the actual map
 	  
 	  markers[i] = new google.maps.Marker({position:markPosition,});
 	  markers[i].setMap(map);
+
+    //Content string used to create infowindow contents
 	  var contentString = 
-	  '<div id="content">'+'<div id="siteNotice">'+'</div>'+'<h1 id="firstHeading" class="firstHeading">'+ js_riverNames[i] + '</h1>'+'<div id="bodyContent">'+'<p> '+ js_riverQuality[i] +' '+ js_riverDanger[i] +'</p>'+'</div>'+'</div>';
+	  '<div id="content">'+'<h1 id="firstHeading" class="firstHeading">'+ js_riverNames[i] + '</h1>'+'<div id="bodyContent">'+'<h3>Directions: </h3>'+ js_directions[i] +'<h3>Address: </h3>'+ js_address[i] +'<h3>Water Quality: </h3>'+ js_riverQuality[i] +'<h3>Water Speed: </h3>'+ js_riverDanger[i] +'</div>'+'</div>';
 	  
 	  infowindows[i] = new google.maps.InfoWindow({content: contentString});
 	  
 	  google.maps.event.addListener(markers[i], 'click', (function(i) {return function() {infowindows[i].open(map,markers[i]);}})(i));//function() {infowindows[i].open(map,markers[i]);});
   }
-  alert(markers[0] + "markers[0]");
   
   //This creates a basic marker object, it just a position and must be set with marker.setMap(map);
-  var marker=new google.maps.Marker({position:myCenter,});
-  
-  //this creates a text window above a marker that already exists, so if you want to create a text window for a marker, put in the content and make sure to pass in the marker when declaring the infowindow
-
-  //This adds a listener that when the user clicks, it will open the infowindow
-  //google.maps.event.addListener(marker, 'click', function() {infowindow.open(map,marker)});
-  
-  //make sure to give the infowindow the marker location when declaring it
-  //infowindow.open(map, marker);
-  //marker.setMap(map);
-  //alert("markers");
-  //for(i = 0; i < markers.length; i++){
-	//  alert(markers[i] + "markers[i]")
-	//  markers[i].setMap(map);
-  //}
-  //alert("markers after");
+  var marker=new google.maps.Marker({position:defaultCenter,});
 }
+
+//Function which handles the lack of geolocation
+function handleNoGeolocation(errorFlag, map)
+{
+  if (errorFlag == true) {
+    //Occurs if browser supports geolocation, but user denies the request
+    alert("Geolocation service failed. We've placed you in Dunedin");
+  }
+  else
+  {
+    //Occurs if browser does not support geolocation
+    alert("Your browser doesn't support geolocation. We've placed you in Dunedin.");
+  }
+  map.setCenter(defaultCenter);
+}
+
 //Add a DOM listener that will execute the initialize() function on window load
 google.maps.event.addDomListener(window, 'load', initialize); 
