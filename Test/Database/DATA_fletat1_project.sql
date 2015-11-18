7d78e562b68bf6d8cd9862e0c7ab38efb1b59e00
Andrew <andrewf1993@gmail.com>
2015-11-18 16:18:57 +1300

    Added data given by ORC
    Added Locations given by the ORC.

--- Test/Database/DATA_fletat1_project.sql
+++ Test/Database/DATA_fletat1_project.sql
@@ -1,6 +1,7 @@
 INSERT INTO WaterSource (riverName, quality, danger) VALUES ('Tokomairiro River', '3', '2');
 INSERT INTO WaterSource (riverName, quality, danger) VALUES ('Clutha River', '2', '5');
 INSERT INTO WaterSource (riverName, quality, danger) VALUES ('Leith River', '1', '4');
+INSERT INTO WaterSource (riverName, quality, danger) VALUES ('Taieri River', '4', '3');
 
 INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('1', '-46.154473', '169.977334', 'Swimming and Fishing areas can be accessed from either side of the bridge.', '218 Coal Gully Rd');
 INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('1', '-46.159034', '169.977891', 'Swimming and Fishing areas can only be accessed from within the paddock. Ask for permission from neighbouring housing.', '390 Toko Mouth Rd');
@@ -8,3 +9,7 @@ INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) V
 INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('2', '-46.278193', '169.795598', 'NO SWIMMING. The Clutha River is extremely dangerous.', 'Near 262 Riverbank Rd');
 INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('3', '-45.867411', '170.517797', 'NO SWIMMING. The Leith River is unsanitary.', 'Near 12 Riego St');
 INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('3', '-45.850303', '170.503578', 'NO SWIMMING. The Leith River is unsanitary.', 'Near 5 Rockside Rd');
+INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('4', '-45.851388', '170.242777', 'Taieri at Outram. Swimming area can be accessed from side of road.', 'Near 10 George King Memorial Dr');
+INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('4', '-45.258888', '170.271666', 'Taieri at Tiroiti. Heading North on Kyeburn-Hyde Rd, either turn right onto Horseburn Rd, or continue North approximately 300m for gravel carpark on right.', 'Near 9750 Kyeburn-Hyde Rd');
+INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('4', '-45.198333', '170.153055', 'Taieri at Waipiata. Swimming area can be access through gate on corner of Patearoa-Waipiata Rd and Waipiata-Kokonga Rd.', 'On corner of Patearoa-Waipiata Rd and Waipiata-Kokonga Rd.');
+INSERT INTO Location (waterSourceID, latitude, longitude, directions, address) VALUES ('4', '-45.570277', '170.140277', 'Taieri at Sutton. NO SWIMMING. Private Property', 'Near 520 Murrays Rd.');
